c4ab1590eb448feba2ae921d9403428c6c0f5589
Gustavo Caminiti <gu.caminiti@gmail.com>
2025-02-13 15:21:52 -0300

    Adicionando script.js stule.css e atualizando index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script para ativar/desativar o menu no mobile
+document.getElementById("menu-btn").addEventListener("click", function () {
+    document.querySelector("aside").classList.toggle("hidden");
+});
